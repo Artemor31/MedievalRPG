d38826949c49f5603b6a38b20ec83ab004c5ceb1
Artem <bogunovartyom@yandex.ru>
2022-05-04 17:24:56 +0300

    init commit 2

---
+++ MedievalRPG/Assets/CodeBase/Services/Ads/AdsService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace CodeBase.Services.Ads
+{
+  public class AdsService : IAdsService, IUnityAdsListener
+  {
+    private const string AndroidGameId = "4106937";
+    private const string IOSGameId = "4106936";
+
+    private const string UnityRewardedVideoIdAndroid = "Rewarded_Android";
+    private const string UnityRewardedVideoIdIOS = "Rewarded_iOS";
+
+    private string _gameId;
+    private string _placementId;
+    
+    private Action _onVideoFinished;
+
+    public event Action RewardedVideoReady;
+
+    public int Reward => 15;
+
+    public void Initialize()
+    {
+      SetIdsForCurrentPlatform();
+      Advertisement.AddListener(this);
+      Advertisement.Initialize(_gameId);
+    }
+
+    public void ShowRewardedVideo(Action onVideoFinished)
+    {
+      _onVideoFinished = onVideoFinished;
+      Advertisement.Show(_placementId);
+    }
+
+    public bool IsRewardedVideoReady => Advertisement.IsReady(_placementId);
+
+    public void OnUnityAdsReady(string placementId)
+    {
+      Debug.Log($"OnUnityAdsReady {placementId}");
+
+      if (placementId == _placementId) 
+        RewardedVideoReady?.Invoke();
+    }
+
+    public void OnUnityAdsDidError(string message) => 
+      Debug.Log($"OnUnityAdsDidError {message}");
+
+    public void OnUnityAdsDidStart(string placementId) => 
+      Debug.Log($"OnUnityAdsDidStart {placementId}");
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+      switch (showResult)
+      {
+        case ShowResult.Failed:
+          Debug.LogError($"OnUnityAdsDidFinish {showResult}");
+          break;
+        case ShowResult.Skipped:
+          Debug.LogError($"OnUnityAdsDidFinish {showResult}");
+          break;
+        case ShowResult.Finished:
+          _onVideoFinished?.Invoke();
+          break;
+        default:
+          Debug.LogError($"OnUnityAdsDidFinish {showResult}");
+          break;
+      }
+
+      _onVideoFinished = null;
+    }
+
+    private void SetIdsForCurrentPlatform()
+    {
+      switch (Application.platform)
+      {
+        case RuntimePlatform.Android:
+          _gameId = AndroidGameId;
+          _placementId = UnityRewardedVideoIdAndroid;
+          break;
+
+        case RuntimePlatform.IPhonePlayer:
+          _gameId = IOSGameId;
+          _placementId = UnityRewardedVideoIdIOS;
+          break;
+
+        case RuntimePlatform.WindowsEditor:
+          _gameId = IOSGameId;
+          _placementId = UnityRewardedVideoIdIOS;
+          break;
+
+        default:
+          Debug.Log("Unsupported platform for ads.");
+          break;
+      }
+    }
+  }
+}
---
+++ MedievalRPG/Assets/CodeBase/Services/Ads/IAdsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeBase.Services.Ads
+{
+  public interface IAdsService : IService
+  {
+    event Action RewardedVideoReady;
+    bool IsRewardedVideoReady { get; }
+    int Reward { get; }
+    void Initialize();
+    void ShowRewardedVideo(Action onVideoFinished);
+  }
+}
